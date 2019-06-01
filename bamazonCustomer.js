23b4746c31c88ed95fd606e77063902953318d47
Trash Panda <d.george.schlosser@gmail.com>
2019-05-31 21:05:35 -0400

    initial commit: necessary files created, json initialized, npm installed, infrastructure to protect pw, pseudocode in main .js & .sql files

---
+++ bamazonCustomer.js
@@ -0,0 +1,11 @@
+// running app displays all items for sale, including: ids, names, and prices
+
+// 2 prompts
+// ask for ID of product they'd like to buy
+
+// ask for quantity
+    // err if not enough: log message & prevent order
+
+    // if enough: fill order:
+        // update db quantity
+        // log total cost
---
+++ bamazonSchema.sql
@@ -0,0 +1,13 @@
+-- database = bamazon
+    -- table inside = products
+        -- products contains following columns:
+        -- item_id (unique id for each product)
+        -- product_name (Name of product)
+        -- department_name
+        -- price (cost to customer)
+        -- stock_quantity (how much of the product is  available in stores)
+
+
+
+
+-- Seeds?: Create 10 mock products
