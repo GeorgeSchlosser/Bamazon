f64a4ddcf70eaccb0d5a5ec77f2b91e66024ae6d
Trash Panda <d.george.schlosser@gmail.com>
2019-06-03 18:01:13 -0400

    established connection between bamazonCustomer.js & MySQL db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,28 @@
+// requirements
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// link to MySQL
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "str1d3r",
+    database: "bamazon"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+  });
+
 // running app displays all items for sale, including: ids, names, and prices
 
 // 2 prompts
