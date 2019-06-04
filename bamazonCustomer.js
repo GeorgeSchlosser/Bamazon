3c15cac62f300453c6596f41b85b6ea9246d7f89
Trash Panda <d.george.schlosser@gmail.com>
2019-06-04 15:08:39 -0400

    running app displays all items for sale w/ individual id,product name, & price

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,10 +20,21 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    connection.end();
+    postConnection();
+    // connection.end();
   });
 
 // running app displays all items for sale, including: ids, names, and prices
+function postConnection() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
+      }
+    //   console.log(res);
+      connection.end();
+    });
+  }
 
 // 2 prompts
 // ask for ID of product they'd like to buy
