2c692e40c839b2f2ec8f1f2f3e7e46f5dc77ed35
Trash Panda <d.george.schlosser@gmail.com>
2019-06-06 15:45:44 -0400

    began cleaining code to solve placing an order, intending to create separate functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,27 +15,30 @@ var connection = mysql.createConnection({
     // Your password
     password: "str1d3r",
     database: "bamazon"
-  });
+});
   
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    start();
+    displayItems();
     // connection.end();
   });
 
 // running app displays all items for sale, including: ids, names, and prices
-function start() {
+function displayItems() {
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
       for (var i = 0; i < res.length; i++) {
         console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
       }
-      console.log(res);
+    });
+};
+    //   console.log(res);
     //   connection.end();
     // });
-    inquirer
-        .prompt([
+
+userPrompt = () => {    
+    inquirer.prompt([
             {
                 name: "buyItems",
                 type: "rawlist",
@@ -49,12 +52,41 @@ function start() {
                 message: "Choose the ID of the item you would like to buy"
             },
             {
-                
+                name: "quantity",
+                type: "input",
+                message: "How many would you like to buy?"
             }
-        ]);
+        ])
+        .then(function(answer) {
+            // get info of selected item
+            var selectedItem;
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].item_id === answer.choice) {
+                    selectedItem = res[i];
+                    console.log(selectedItem);
+                }
+            }
+            console.log(selectedItem);
+            // check if suffcient quantity
+            if (selectedItem.stock_quantity < parseInt(answer.quantity)) {
+                console.log("Sorry, we don't have enough in stock to place your order. Try again.");
+                start();
+            }
+            else {
+                update();
+            }
+
+        }
+        )
 
     });
 }
+
+// update = () => {
+//     connection.query("UPDATE products SET ? WHERE?",
+//     [{selectedItem.stock_quantity: res[0].stock_quantity - answer.quantity}])
+
+// }
 // 2 prompts
 // ask for ID of product they'd like to buy
 
